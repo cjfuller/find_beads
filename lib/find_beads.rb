d06bad68281d2b1293bbe3979ac1744f46bf178f
Colin J. Fuller <cjfuller@gmail.com>
2013-06-20 14:24:36 -0700

    cleaned up a lot of unnecessary annoying whitespace

--- lib/find_beads.rb
+++ lib/find_beads.rb
@@ -29,19 +29,14 @@ require 'rimageanalysistools/get_image'
 require 'rimageanalysistools/image_shortcuts'
 require 'rimageanalysistools/create_parameters'
 require 'rimageanalysistools/graythresh'
-
 require 'trollop'
-
 require 'matrix'
-
-java_import Java::edu.stanford.cfuller.imageanalysistools.image.ImageFactory
 
 ##
 # Functions for segmenting and quantifying beads.
 #
 module FindBeads
-
-
+  java_import Java::edu.stanford.cfuller.imageanalysistools.image.ImageFactory
   java_import Java::edu.stanford.cfuller.imageanalysistools.image.ImageCoordinate
   java_import Java::edu.stanford.cfuller.imageanalysistools.image.ImageSet
   java_import Java::edu.stanford.cfuller.imageanalysistools.meta.parameters.ParameterDictionary
@@ -66,30 +61,22 @@ module FindBeads
   #  two-element arrays containing the x,y-coordinates of the centroids of these regions.
   #
   def self.centroids(mask)
-
     cens = {}
 
     mask.each do |ic|
-
       next unless mask[ic] > 0
-      
       cens[mask[ic]] = [0.0, 0.0] unless cens[mask[ic]]
-
       cens[mask[ic]][0] += ic[:x]
       cens[mask[ic]][1] += ic[:y]
-      
     end
 
     h = Histogram.new(mask)
 
     cens.each_key do |k|
-
       cens[k].map! { |e| e / h.getCounts(k) }
-
     end
 
     cens
-
   end
 
 
@@ -109,15 +96,10 @@ module FindBeads
   #  the other points were specified)
   #
   def self.project_point_onto_vector(origin, point_to_project, point_on_line)
-
     unit_vec = (Vector[*point_on_line] - Vector[*origin]).normalize
-
     proj_vec = Vector[*point_to_project] - Vector[*origin]
-
     projected = unit_vec * (proj_vec.inner_product(unit_vec)) + Vector[*origin]
-
     projected.to_a
-
   end
 
 
@@ -132,58 +114,33 @@ module FindBeads
   # @return [Boolean] whether the point is on the border between two regions.
   #
   def self.is_on_voronoi_border?(points, ic)
-
     x = ic[:x]
     y = ic[:y]
-
     closest_index = 0
     next_index = 0
     closest_dist = Float::MAX
     next_dist = Float::MAX
     
-
     points.each_with_index do |p, i|
-
       dist = Math.hypot(p[0] - x, p[1] - y)
 
       if dist < closest_dist then
-
         next_dist = closest_dist
-
         next_index = closest_index
-
         closest_dist = dist
-
         closest_index = i
-
       elsif dist < next_dist then
-
         next_dist = dist
-
         next_index = i
-
       end
-
     end
 
     proj_point = project_point_onto_vector(points[closest_index], [x,y], points[next_index])
-
     next_dist_proj = Math.hypot(points[next_index][0]-proj_point[0], points[next_index][1]-proj_point[1])
     closest_dist_proj = Math.hypot(points[closest_index][0]-proj_point[0], points[closest_index][1]-proj_point[1])
-
-
     cutoff = 1.01*Math.sqrt(2)
 
-    if next_dist_proj - closest_dist_proj < cutoff then
-
-      true
-
-    else 
-
-      false
-
-    end
-
+    (next_dist_proj - closest_dist_proj < cutoff)
   end
 
   ##
@@ -199,27 +156,18 @@ module FindBeads
   # @return [void]
   # 
   def self.recursive_threshold(p, im, mask)
-
     h = Histogram.new(mask)
-
     changed = false
-
     discard_list = {}
 
     1.upto(h.getMaxValue) do |i|
-
       if h.getCounts(i) > p[:max_size].to_i then
-
         values = []
 
         im.each do |ic|
-
           if mask[ic] == i then
-
             values << im[ic]
-
           end
-
         end
 
         thresh = RImageAnalysisTools.graythresh(values)
@@ -227,46 +175,27 @@ module FindBeads
         im.each do |ic|
 
           if mask[ic] == i and im[ic] <= thresh then
-
             mask[ic] = 0
-
             changed = true
-
           end
-
         end
-
       elsif h.getCounts(i) > 0 and h.getCounts(i) < p[:min_size].to_i then
-
         discard_list[i] = true
-
       end
-
     end
 
     im.each do |ic|
-
       if discard_list[im[ic]] then
-
         mask[ic] = 0
-
         changed = true
-        
       end
-
     end
-
 
     if changed then
-
       lf = LabelFilter.new
-
       lf.apply(mask)
-
       recursive_threshold(p, im, mask)
-
     end
-
   end
 
   ##
@@ -276,9 +205,7 @@ module FindBeads
   # @param [Fixnum] rad the radius of the bead in units of pixels.
   #
   def self.calculate_max_size_from_radius(rad)
-
     ((rad+1)**2 * 3.2).to_i
-
   end
 
   ##
@@ -291,9 +218,7 @@ module FindBeads
   # @param @see #calculate_max_size_from_radius
   #
   def self.calculate_min_size_from_radius(rad)
-
     (0.96* (rad+1)**2).to_i
-
   end
 
   ##
@@ -303,7 +228,6 @@ module FindBeads
   # @param [Hash] opts a hash of commandline arguments.
   #
   def self.mask_from_image(im, opts)
-
     seg_ch = nil
     seg_pl = nil
     rad = nil
@@ -320,49 +244,36 @@ module FindBeads
     
     min_size = calculate_min_size_from_radius(rad)
     max_size = calculate_max_size_from_radius(rad)
-    
-    sizes = ImageCoordinate.cloneCoord(im.getDimensionSizes)
 
+    sizes = ImageCoordinate.cloneCoord(im.getDimensionSizes)
     sizes[:c] = 1
     sizes[:z] = 1
 
     im0 = ImageCoordinate.createCoordXYZCT(0,0,0,0,0)
-
     im0[:c] = seg_ch
     im0[:z] = seg_pl
 
     to_seg = im.subImage(sizes, im0).writableInstance
-
     p = RImageAnalysisTools.create_parameter_dictionary(min_size: min_size, max_size: max_size)
-
     im_cp = ImageFactory.create_writable(to_seg)
 
     mstf = MaximumSeparabilityThresholdingFilter.new
-
     lf = LabelFilter.new
-
     saf = SizeAbsoluteFilter.new
 
     filters = [] 
-
     filters << mstf
-
     filters << lf
 
     filters.each do |f|
-
       f.setParameters(p)
       f.setReferenceImage(im_cp)
       f.apply(to_seg)
-
     end
 
     recursive_threshold(p, im_cp, to_seg)
-
     saf.setParameters(p)
-    
     saf.apply(to_seg)
-
     cens = centroids(to_seg)
 
     final_mask = ImageFactory.create_writable(to_seg)
@@ -370,45 +281,30 @@ module FindBeads
     radius = rad
 
     final_mask.each do |ic|
-
       final_mask[ic] = 0
-      
     end
 
     final_mask.each do |ic|
-
       x = ic[:x]
       y = ic[:y]
 
       cens.each_key do |k|
-
         if Math.hypot(cens[k][0] - x, cens[k][1] - y) <= radius then
-
           final_mask[ic] = k
-
         end
-
       end
-
     end
 
-
     final_mask.each do |ic|
-
       next unless final_mask[ic] > 0
 
       if is_on_voronoi_border?(cens.values, ic) then
-
         final_mask[ic] = 0
-
       end
-
     end
 
     lf.apply(final_mask)
-    
     saf.apply(final_mask)
-
     lf.apply(final_mask)
 
     final_mask
@@ -423,36 +319,24 @@ module FindBeads
   # @param [Image] mask the mask to write
   #
   def self.write_output(fn_orig, quant_str, mask)
-
     mask_dir = "output_mask"
-
     quant_dir = "quantification"
-
     mask_ext = "_mask.ome.tif"
-
     quant_ext = "_quant.txt"
 
     dir = File.dirname(fn_orig)
-
     base = File.basename(fn_orig)
-
     base = base.gsub(".ome.tif", "")
-
     mask_dir = File.expand_path(mask_dir, dir)
-
     quant_dir = File.expand_path(quant_dir, dir)
-
     Dir.mkdir(mask_dir) unless Dir.exist?(mask_dir)
     Dir.mkdir(quant_dir) unless Dir.exist?(quant_dir)
 
     mask.writeToFile(File.expand_path(base + mask_ext, mask_dir))
 
     File.open(File.expand_path(base + quant_ext, quant_dir), 'w') do |f|
-
       f.puts(quant_str)
-
     end
-
   end
 
   ##
@@ -462,60 +346,43 @@ module FindBeads
   # @param [Hash] opts a hash of command line options.
   #
   def self.process_file(fn, opts=nil)
-
     puts "processing #{fn}"
 
     im = RImageAnalysisTools.get_image(fn)
-    
     mask = mask_from_image(im, opts)
-
     proj = Java::edu.stanford.cfuller.imageanalysistools.frontend.MaximumIntensityProjection.projectImage(im)
-
     ims = proj.splitChannels
-
     is = ImageSet.new(ParameterDictionary.emptyDictionary)
 
     ims.each do |imc|
-
       is.addImageWithImage(imc)
-
     end
 
     met = IntensityPerPixelMetric.new
-
     q = met.quantify(mask, is)
-
     outdat = Java::edu.stanford.cfuller.imageanalysistools.frontend.LocalAnalysis.generateDataOutputString(q, nil)
-
     write_output(fn, outdat, mask)
-
   end
 
   ##
   # Runs the bead finding on a file or directory, and grabs options from the command line.
   #
   def self.run_find_beads
-
     opts = Trollop::options do
-
       opt :dir, "Directory to process", :type => :string
       opt :file, "File to process", :type => :string
       opt :segchannel, "Channel on which to segment (0-indexed)", :type => :integer, :default => DEFAULT_SEG_CH
       opt :segplane, "Plane on which to segment (0-indexed)", :type => :integer, :default => DEFAULT_SEG_PL
       opt :max_threads, "Maximum number of paralell execution threads", :type => :integer, :default => DEFAULT_THREADS
       opt :beadradius, "Radius of the bead in pixels", :type => :float, :default => DEFAULT_BEAD_RADIUS
-
     end
 
     if opts[:dir] then
-
       fod = opts[:dir]
-
       sleep_time_s = 0.5
       threads = []
 
       Dir.foreach(fod) do |f|
-
         until threads.count { |t| t.alive? } < opts[:max_threads] do
           sleep sleep_time_s
         end
@@ -523,39 +390,24 @@ module FindBeads
         fn = File.expand_path(f, fod)
 
         if File.file?(fn) then
-
           begin
-
             threads << Thread.new do 
-
               process_file(fn, opts)
-
             end
 
           rescue Exception => e
-
             puts "Unable to process #{fn}:"
             puts e.message
-
           end
-          
         end
-
       end
 
       threads.each { |t| t.join }
-
     end
-
-
 
     if opts[:file] then
-
       process_file(opts[:file], opts)
-      
     end
-
   end
-
 end
 
